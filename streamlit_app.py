309aae0bdbeb835ad4dfc35ab915ff7bd484734a
Urja99 <urja.yadav@tigeranalytics.com>
2025-07-29 07:57:51 +0000

    Updating main file content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+# Sidebar
+st.sidebar.title("Sidebar")
+st.sidebar.write("This is the sidebar. You can add filters or navigation here.")
+
+
+st.title("🎈 My Streamlit Deployment app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.set_page_config(
+    page_title="Streamlit Deployment app",
+    page_icon="rocket",
+)
+
